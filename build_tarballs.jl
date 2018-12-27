1fe95b833a5dbdc34fa28e9ce16001aa4a4d46dd
Juan Pablo Vielma <jvielma@mit.edu>
2018-12-27 18:14:33 -0300

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -21,18 +21,38 @@ cd Cbc-releases-2.9.9/
 update_configure_scripts
 mkdir build
 cd build/
+## STATIC BUILD START
 ../configure --prefix=$prefix --with-pic --disable-pkg-config --host=${target} --enable-shared --disable-static \
 --enable-dependency-linking lt_cv_deplibs_check_method=pass_all \
 --with-cgl-lib="-L${prefix}/lib -lCgl" --with-cgl-incdir="$prefix/include/coin" \
 --with-asl-lib="-L${prefix}/lib -lasl" --with-asl-incdir="$prefix/include/asl" \
---with-blas="-L${prefix}/lib -lcoinblas" \
---with-lapack="-L${prefix}/lib -lcoinlapack" \
+--with-blas-lib="-L${prefix}/lib -lcoinblas" \
+--with-lapack-lib="-L${prefix}/lib -lcoinlapack" \
 --with-metis-lib="-L${prefix}/lib -lcoinmetis" --with-metis-incdir="$prefix/include/coin/ThirdParty" \
 --with-mumps-lib="-L${prefix}/lib -lcoinmumps" --with-mumps-incdir="$prefix/include/coin/ThirdParty" \
 --with-coinutils-lib="-L${prefix}/lib -lCoinUtils" --with-coinutils-incdir="$prefix/include/coin" \
---with-osi-lib="-L${prefix}/lib -lOsi"  --with-osi-incdir="$prefix/include/coin" \
---with-clp-lib="-L${prefix}/lib -lClp -lOsiClp" --with-clp-incdir="$prefix/include/coin" \
---with-coindepend-lib="-L${prefix}/lib -lCgl -lOsi -lClp -lCoinUtils"
+--with-osi-lib="-L${prefix}/lib -lOsi" --with-osi-incdir="$prefix/include/coin" \
+  LDFLAGS=-ldl;
+--with-clp-lib="-L${prefix}/lib -lClp -lOsiClp" --with-clp-incdir="$prefix/include/coin" #\
+#--with-coindepend-lib="-L${prefix}/lib -lCgl -lOsi -lClp -lCoinUtils"
+## STATIC BUILD END
+
+
+## DYNAMIC BUILD START
+#../configure --prefix=$prefix --with-pic --disable-pkg-config --host=${target} --enable-shared --disable-static \
+#--enable-dependency-linking lt_cv_deplibs_check_method=pass_all \
+#--with-cgl-lib="-L${prefix}/lib -lCgl" --with-cgl-incdir="$prefix/include/coin" \
+#--with-asl-lib="-L${prefix}/lib -lasl" --with-asl-incdir="$prefix/include/asl" \
+#--with-blas="-L${prefix}/lib -lcoinblas" \
+#--with-lapack="-L${prefix}/lib -lcoinlapack" \
+#--with-metis-lib="-L${prefix}/lib -lcoinmetis" --with-metis-incdir="$prefix/include/coin/ThirdParty" \
+#--with-mumps-lib="-L${prefix}/lib -lcoinmumps" --with-mumps-incdir="$prefix/include/coin/ThirdParty" \
+#--with-coinutils-lib="-L${prefix}/lib -lCoinUtils" --with-coinutils-incdir="$prefix/include/coin" \
+#--with-osi-lib="-L${prefix}/lib -lOsi"  --with-osi-incdir="$prefix/include/coin" \
+#--with-clp-lib="-L${prefix}/lib -lClp -lOsiClp" --with-clp-incdir="$prefix/include/coin" \
+#--with-coindepend-lib="-L${prefix}/lib -lCgl -lOsi -lClp -lCoinUtils"
+## DYNAMIC BUILD END
+
 make -j${nproc}
 make install
 
@@ -63,15 +83,15 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/juan-pablo-vielma/CglBuilder/releases/download/v0.59.10-1/build_CglBuilder.v0.59.10.jl",
-    "https://github.com/JuliaOpt/ClpBuilder/releases/download/v1.16.11-1/build_ClpBuilder.v1.16.11.jl",
-    "https://github.com/juan-pablo-vielma/OsiBuilder/releases/download/v0.107.9-1/build_OsiBuilder.v0.107.9.jl",
-    "https://github.com/juan-pablo-vielma/CoinUtilsBuilder/releases/download/v2.10.14-1/build_CoinUtilsBuilder.v2.10.14.jl",
-    "https://github.com/juan-pablo-vielma/COINMumpsBuilder/releases/download/v1.6.0-1/build_COINMumpsBuilder.v1.6.0.jl",
-    "https://github.com/juan-pablo-vielma/COINMetisBuilder/releases/download/v1.3.5-1/build_COINMetisBuilder.v1.3.5.jl",
-    "https://github.com/juan-pablo-vielma/COINLapackBuilder/releases/download/v1.5.6-1/build_COINLapackBuilder.v1.5.6.jl",
-    "https://github.com/juan-pablo-vielma/COINBLASBuilder/releases/download/v1.4.6-1/build_COINBLASBuilder.v1.4.6.jl",
-    "https://github.com/juan-pablo-vielma/ASLBuilder/releases/download/v3.1.0-1/build_ASLBuilder.v3.1.0.jl"
+    "https://github.com/juan-pablo-vielma/CglBuilder/releases/download/v0.59.10-1-static/build_CglBuilder.v0.59.10.jl",
+    "https://github.com/JuliaOpt/ClpBuilder/releases/download/v1.16.11-1-static/build_ClpBuilder.v1.16.11.jl",
+    "https://github.com/juan-pablo-vielma/OsiBuilder/releases/download/v0.107.9-1-static/build_OsiBuilder.v0.107.9.jl",
+    "https://github.com/juan-pablo-vielma/CoinUtilsBuilder/releases/download/v2.10.14-1-static/build_CoinUtilsBuilder.v2.10.14.jl",
+    "https://github.com/juan-pablo-vielma/COINMumpsBuilder/releases/download/v1.6.0-1-static/build_COINMumpsBuilder.v1.6.0.jl",
+    "https://github.com/juan-pablo-vielma/COINMetisBuilder/releases/download/v1.3.5-1-static/build_COINMetisBuilder.v1.3.5.jl",
+    "https://github.com/juan-pablo-vielma/COINLapackBuilder/releases/download/v1.5.6-1-static/build_COINLapackBuilder.v1.5.6.jl",
+    "https://github.com/juan-pablo-vielma/COINBLASBuilder/releases/download/v1.4.6-1-static/build_COINBLASBuilder.v1.4.6.jl",
+    "https://github.com/juan-pablo-vielma/ASLBuilder/releases/download/v3.1.0-1-static/build_ASLBuilder.v3.1.0.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
